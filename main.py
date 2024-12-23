28c196fba3da9ecfef890604527ad49d7a1dcf45
JoelSasot <joel.sasot@estudiantat.upc.edu>
2024-12-23 18:11:48 +0100

    removal bar

--- main.py
+++ main.py
@@ -2,17 +2,9 @@ import streamlit as st
 from functions.header import print_header
 st.set_page_config(layout='wide')
 
-
 st.markdown("""
 <style>
     #MainMenu, header, footer {visibility: hidden;}
-
-    /* This code gets the first element on the sidebar,
-    and overrides its default styling */
-    section[data-testid="stSidebar"] div:first-child {
-        top: 0;
-        height: 0vh;
-    }
 </style>
 """,unsafe_allow_html=True)
 
