e1046afc38cbf1f955063e93fae074d3b96bd6d9
JoelSasot <joel.sasot@estudiantat.upc.edu>
2024-12-23 18:16:44 +0100

    hide header

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from functions.header import print_header
 st.set_page_config(layout='wide')
-st.write('<style>div.block-container{padding-top:0.8rem;} </style>', unsafe_allow_html=True   )
+st.write('''
+         <style>
+         div.block-container{padding-top:0.8rem;} 
+         header {visibility: hidden;}
+         </style>''', unsafe_allow_html=True   )
 
 print_header()
