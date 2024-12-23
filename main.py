89ae6a702ba3ce7eaca315c5d8eec32f7b511085
JoelSasot <joel.sasot@estudiantat.upc.edu>
2024-12-23 18:14:09 +0100

    predefined collapser joel

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import streamlit as st
 from functions.header import print_header
 st.set_page_config(layout='wide')
-
-st.markdown("""
-<style>
-    #MainMenu, header, footer {visibility: hidden;}
-</style>
-""",unsafe_allow_html=True)
+st.write('<style>div.block-container{padding-top:0.8rem;} </style>', unsafe_allow_html=True   )
 
 print_header()
