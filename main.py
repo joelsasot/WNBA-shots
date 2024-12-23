fd70397dc88f08ae0bca366c0f326a89bf4be45e
JoelSasot <joel.sasot@estudiantat.upc.edu>
2024-12-23 18:09:34 +0100

    check remove white space

--- main.py
+++ main.py
@@ -2,4 +2,18 @@ import streamlit as st
 from functions.header import print_header
 st.set_page_config(layout='wide')
 
+
+st.markdown("""
+<style>
+    #MainMenu, header, footer {visibility: hidden;}
+
+    /* This code gets the first element on the sidebar,
+    and overrides its default styling */
+    section[data-testid="stSidebar"] div:first-child {
+        top: 0;
+        height: 100vh;
+    }
+</style>
+""",unsafe_allow_html=True)
+
 print_header()
